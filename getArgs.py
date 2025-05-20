b1af018dd1dae7568ce923e97bbab877b4642639
Minguri <132596722+Minguri06@users.noreply.github.com>
2025-05-21 01:37:57 +0900

    This file will be my first python module!
    This file is get Argument module with python

---
+++ getArgs.py
@@ -0,0 +1,9 @@
+#파이썬 인자값을 전달하는 모듈/sending python args
+
+import argparse
+
+parser = argparse.ArgumentParser(description='args')
+parser.add_argument('-w',type=str,help='Enter the wordlist PATH')
+parser.add_argument('-u',type=str,help='Enter the URL')
+getpars = parser.parse_args()
+print(getpars)
